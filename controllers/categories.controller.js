757957f67af15c13e51323318961f8edf780bb11
Ahmed Imam <ahmedsamir_imam@hotmail.com>
2020-04-07 18:28:48 +0300

    Added code for GET 1 Category, Test is passing

--- controllers/categories.controller.js
+++ controllers/categories.controller.js
@@ -10,3 +10,20 @@ exports.findAll = (req, res) => {
       else res.send(data);
     });
   };
+
+
+exports.findOne = (req, res) => {
+    Category.findById(req.params.categoryId, (err, data) => {
+    if (err) {
+      if (err.kind === "not_found") {
+        res.status(404).send({
+          message: `Not found Category with id ${req.params.categoryId}`
+        });
+      } else {
+        res.status(500).send({
+          message: "Error retrieving Category with id " + req.params.categoryId
+        });
+      }
+    } else res.send(data);
+  });
+};
--- routes/category.routes.js
+++ routes/category.routes.js
@@ -2,6 +2,7 @@ module.exports = app => {
     const categories = require("../controllers/categories.controller.js");
   
     app.get("/api/categories", categories.findAll);
-  
+    app.get("/api/categories/:categoryId", categories.findOne);
+
 
   };
