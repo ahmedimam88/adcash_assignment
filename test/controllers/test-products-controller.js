acbd893fc96bb410409ea14030bf869a968ed270
Ahmed Imam <ahmedsamir_imam@hotmail.com>
2020-04-07 20:19:00 +0300

    Added Unit Tests GET specific products

--- test/controllers/test-products-controller.js
+++ test/controllers/test-products-controller.js
@@ -18,4 +18,26 @@ describe("GET /api/products", () => {
       expect(res.body[0]).to.have.property("productcode");
     });
   });
+
+  describe("GET /api/products/:id", () => {
+    it("List Specific product with Success", async () => {
+  
+      var resp = await request(app).get("/api/products");
+      const res = await request(app).get("/api/products/" + resp.body[0].id);
+      
+      expect(res.status).to.equal(200);
+      expect(res.body).to.have.property("id", resp.body[0].id);
+      expect(res.body).to.have.property("productcode", resp.body[0].productcode);
+    });
+  });
+  
+  
+  describe("GET /api/products/:id", () => {
+    it("List Specific product with Failure", async () => {
+  
+      const res = await request(app).get("/api/products/2121");
+      
+      expect(res.status).to.equal(404);
+    });
+  });
   
